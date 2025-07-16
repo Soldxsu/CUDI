ebeaa0b6034f1911bbc62ebc923c608cad89a2d2
vicky <victoriapuig55@gmail.com>
2025-07-16 15:23:57 -0300

    nuevo diseño de login

--- login.php
+++ login.php
@@ -30,84 +30,40 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
     <title>Inicio de Sesión</title>
-    <style>
-        body {
-            background: linear-gradient(to right, #3B6CDC, #6BD4E2);
-            justify-content: center;
-            justify-items: center;
-            font-family: 'Josefin Sans';
-        }
-
-        img {
-            position: relative;
-            width: 190px;
-            height: 135px;
-            margin-top: 20px;
-        }
-
-        .formulario {
-            background-color: white;
-            border-radius: 8px;
-            width: 486px;
-            height: 261px;
-            justify-content: center;
-            justify-items: center;
-            flex-direction: column;
-            padding: 8px 0;
-            display: grid;
-            grid-template-columns: repeat(auto-fit, 1fr);
-        }
-
-        .label {
-            color: rgb(0, 0, 0);
-        }
-
-        .input {
-            border-radius: 4px;
-            width: 450px;
-            height: 30px;
-            position: relative;
-            margin-top: 5px;
-            border: 1px solid;
-        }
-
-        .h1 {
-            font-style: 'Josefin Sans';
-            color: white;
-            font-size: 50px;
-        }
-
-        #enviar {
-            background-color: #2a5ed6;
-            width: 458px;
-            position: relative;
-            top: 35px;
-            height: 40px;
-            border-radius: 4px;
-            border: 1px solid;
-            border-color: black;
-            color: white;
-        }
-    </style>
 </head>
 <body>
-    <img src="img/logo.png" alt="logo">
-    <h1 class="h1">Inicio de Sesión</h1>
-    <?php if (!empty($error)) echo "<h3 style='color:red;'>$error</h3>"; ?>
+    <div class="background">
+        <svg width="837" height="1024" viewBox="0 0 837 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M837 0C837 0 606.5 210.06 606.5 520.806C606.5 822.722 837 1024 837 1024H0V0H837Z" fill="url(#paint0_linear_103_63)"/>
+            <defs>
+                <linearGradient id="paint0_linear_103_63" x1="418.5" y1="0" x2="418.5" y2="1024" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#5F61D4"/>
+                <stop offset="0.5" stop-color="#4CA6F1"/>
+                <stop offset="1" stop-color="#65DCD2"/>
+                </linearGradient>
+            </defs>
+        </svg>
+    </div>
+
+    <div class="imagen">
+        <img src="img/Cudi.png" alt="logo del cudi">
+    </div>
+
     <div class="formulario">
+        <h1>Inicio de Sesión</h1>
+        <?php if (!empty($error)) echo "<h3 style='color:red;'>$error</h3>"; ?>
         <form action="login.php" method="post">
-            <br>
-            <label class="label">Usuario</label>
-            <br>
-            <input class="input" type="text" name="username" placeholder="Valor" required>
-            <br><br><br>
-            <label class="label">Contraseña</label>
-            <br>
-            <input class="input" type="password" name="password" placeholder="Valor" required>
-            <br>
+            <input class="input" type="text" name="username" placeholder="Usuario" required>
+            <input class="input" type="password" name="password" placeholder="Contraseña" required>
             <input id="enviar" type="submit" value="Iniciar Sesión">
         </form>
     </div>
 </body>
+
 </html>
